1f34114c000ebf6a10537738334e114bd52883da
zhengguo <hzgmymail@163.com>
2024-05-29 17:05:10 +0800

    添加新工程
    添加新工程 Samples

---
+++ Samples/Pods/Target Support Files/Pods-Samples/Pods-Samples-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Samples : NSObject
+@end
+@implementation PodsDummy_Pods_Samples
+@end
---
+++ Samples/Samples/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Samples
+//
+//  Created by Zhengguo Huang on 2024/5/26.
+//
+
+import UIKit
+import HzgTools
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        self.tableView.delegate = self
+//        self.tableView.dataSource = self
+    }
+    
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        return 1
+//    }
+//    
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+////        <#code#>
+//    }
+//    
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 60.0
+//    }
+
+}
+

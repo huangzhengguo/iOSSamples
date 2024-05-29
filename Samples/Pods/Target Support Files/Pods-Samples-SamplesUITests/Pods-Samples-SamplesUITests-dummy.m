d81253c639d6d79b996e90cc7cce7500a7c197d4
zhengguo <hzgmymail@163.com>
2024-05-29 17:08:34 +0800

    添加 .gitignore 文件
    添加 .gitignore 文件

--- Samples/Pods/Target Support Files/Pods-Samples-SamplesUITests/Pods-Samples-SamplesUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Samples_SamplesUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Samples_SamplesUITests
-@end
--- Samples/Pods/Target Support Files/Pods-Samples/Pods-Samples-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Samples : NSObject
-@end
-@implementation PodsDummy_Pods_Samples
-@end
